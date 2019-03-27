2435183e242e1a6dd31aabda24b86644e3722ccd
TamYen <34823904+TamYen@users.noreply.github.com>
2019-03-27 14:27:37 +0700

    init project

---
+++ MyPham/Controllers/AdLoaiSPController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyPham.Models;
+using PagedList.Mvc;
+using PagedList;
+
+namespace MyPham.Controllers
+{
+    public class AdLoaiSPController : Controller
+    {
+        DbMyPhamDataContext db = new DbMyPhamDataContext();
+        // GET: AdLoaiSP
+        public ActionResult QLloaisp()
+        {
+            if (Session["TaikhoanAdmin"] == null)
+            {
+                login();
+                return null;
+            }
+
+            return View(db.LoaiSPs.OrderBy(n => n.TenLoai));
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(LoaiSP l)
+        {
+            if (Session["TaikhoanAdmin"] == null)
+            {
+                login();
+                return null;
+            }
+            db.LoaiSPs.InsertOnSubmit(l);
+            db.SubmitChanges();
+            return RedirectToAction("QLloaiSP");
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            if (Session["TaikhoanAdmin"] == null)
+            {
+                login();
+                return null;
+            }
+            LoaiSP l = db.LoaiSPs.SingleOrDefault(n => n.MaLoai == id);
+            if(l == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View();
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Xoa(int id)
+        {
+            LoaiSP l = db.LoaiSPs.SingleOrDefault(n => n.MaLoai == id);
+            if(l == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.LoaiSPs.DeleteOnSubmit(l);
+            db.SubmitChanges();
+            return RedirectToAction("QLloaiSP");
+        }
+
+        public void login()
+        {
+            Response.StatusCode = 404;
+            //return null;
+        }
+    }
+}
---
+++ MyPham/Controllers/MyphamController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_DoAn.Controllers;
+using MyPham.Models;
+using PagedList;
+using PagedList.Mvc;
+using Microsoft.Ajax.Utilities;
+
+namespace MVC_DoAn.Controllers
+{
+    public class MyPhamController : Controller
+    {
+        DbMyPhamDataContext data = new DbMyPhamDataContext();
+        private List<SanPham> Ar1;
+        private List<SanPham> Ar2;
+
+        private List<SanPham> LayHangMoi(int count1, int count2)
+        {
+            List<SanPham> ar = new List<SanPham>();
+            Ar1 = data.SanPhams.OrderByDescending(a => a.NgayCapNhat).Take(count1).ToList();
+
+            Ar2 = data.SanPhams.OrderByDescending(a => a.GiaBan).Take(count2).ToList();
+            
+            foreach (var a in Ar1)
+            {
+                ar.Add(a);
+            }
+            foreach (var a in Ar2)
+            {
+                ar.Add(a);
+            }
+            return ar;
+
+        }
+
+
+        // GET: MyPham
+        public ActionResult Index()
+        {
+            var hangmoi = LayHangMoi(6, 6);
+            return View(hangmoi);
+        }
+
+        public ActionResult SPdacbiet()
+        {
+            var s = data.SanPhams.OrderByDescending(n => n.GiaBan).Take(1);
+            return View(s);
+        }
+
+        public ActionResult SPdacbiets(int? page)
+        {
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
+            var s = data.SanPhams.OrderByDescending(n => n.GiaBan).Take(36);
+            return View(s.ToPagedList(pageNum, pageSize));
+        }
+
+        public ActionResult LoaiSP()
+        {
+            var loaiSP = from a in data.LoaiSPs select a;
+            return PartialView(loaiSP);
+        }
+        public ActionResult NhaSX()
+        {
+            var nsx = from a in data.NhaSanXuats select a;
+            return PartialView(nsx);
+        }
+
+        public ActionResult SpTheoLoai(int id, int ? page)
+        {
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
+            var sp = from s in data.SanPhams.OrderByDescending(a=>a.NgayCapNhat) where s.MaLoai == id select s;
+            return View(sp.ToPagedList(pageNum,pageSize));
+        }
+
+        public ActionResult SpTheoNSX(int id, int? page)
+        {
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
+            var sp = from s in data.SanPhams.OrderByDescending(a => a.NgayCapNhat) where s.MaNSX == id select s;
+            //ViewBag.ten = (from t in data.NhaSanXuats where t.MaNSX == id select t.TenNSX).ToString();
+            return View(sp.ToPagedList(pageNum,pageSize));
+        }
+
+        public ActionResult Details(int id)
+        {
+            var sp = from s in data.SanPhams where s.MaSP == id select s;
+            return View(sp.Single());
+        }
+        
+        [HttpPost]
+        public ActionResult KQtimkiem(string Search, int? page)
+        {
+
+            List<SanPham> kqTk = data.SanPhams.Where(n => n.TenSP.Contains(Search)).ToList();
+
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
+
+            if (kqTk.Count == 0)
+            {
+                ViewBag.KQtk = kqTk.Count.ToString();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.KQtk = kqTk.Count.ToString();
+                return View(kqTk.OrderBy(n => n.TenSP).ToPagedList(pageNum, pageSize));
+            }
+        }
+    }
+}
